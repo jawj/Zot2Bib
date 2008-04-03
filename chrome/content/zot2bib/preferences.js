fd052ab0895299c5d9005b119f0a41abced398a4
George MacKerron <george@mackerron.co.uk>
2008-04-03 11:59:56 +0000

    Updates

--- chrome/content/zot2bib/preferences.js
+++ chrome/content/zot2bib/preferences.js
@@ -11,44 +11,45 @@ function listAction(e) {
   var downbtn = gebi('z2b-move-down');
 
   if (e && e.type == 'command' ) {
-var olditem, newitem;
+
+  var olditem, newitem;
+
   if (e.target == addbtn) {
-      var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-      fp.init(window, "Choose BibTeX file", nsIFilePicker.modeOpen);
-      fp.appendFilter("BibTeX", "*.bib");
-      var rv = fp.show();
-      if (rv == nsIFilePicker.returnOK) {
-        var path = fp.file.path;
-        for (var i = 0; i < listbox.getRowCount(); i ++) {
-          olditem = listbox.getItemAtIndex(i);
-          if (olditem.label == path) {
-            newitem = olditem;
-            break;
-          }
+    var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+    fp.init(window, "Choose BibTeX file", nsIFilePicker.modeOpen);
+    fp.appendFilter("BibTeX", "*.bib");
+    var rv = fp.show();
+    if (rv == nsIFilePicker.returnOK) {
+      var path = fp.file.path;
+      for (var i = 0; i < listbox.getRowCount(); i ++) {
+        olditem = listbox.getItemAtIndex(i);
+        if (olditem.label == path) {
+          newitem = olditem;
+          break;
         }
-        if (! newitem) newitem = listbox.appendItem(path);
       }
-
-    } else if (e.target == removebtn) {
-      listbox.removeItemAt(listbox.selectedIndex);
-
-    } else if (e.target == upbtn || e.target == downbtn) {
-      var newindex = listbox.selectedIndex + ( e.target == upbtn ? -1 : 1 );
-      olditem = listbox.removeItemAt(listbox.selectedIndex);
-      newitem = newindex < listbox.getRowCount() ? listbox.insertItemAt(newindex, olditem.label) : listbox.appendItem(olditem.label);
+      if (! newitem) newitem = listbox.appendItem(path);
     }
-if (newitem) {
-        listbox.selectItem(newitem);
-        listbox.ensureElementIsVisible(newitem);
-}
 
+  } else if (e.target == removebtn) {
+    listbox.removeItemAt(listbox.selectedIndex);
+
+  } else if (e.target == upbtn || e.target == downbtn) {
+    olditem = listbox.selectedItem;
+    newitem = listbox.getItemAt(listbox.selectedIndex + ( e.target == upbtn ? -1 : 1 ));
+    [olditem.label, newitem.label] = [newitem.label, olditem.label];
+  }
+
+  if (newitem) {
+    listbox.selectItem(newitem);
+    listbox.ensureElementIsVisible(newitem);
+  }
   copyListToPrefs();
+  }
 
-}
   removebtn.disabled = ! listbox.selectedItem;
   upbtn.disabled = ! listbox.selectedItem || listbox.selectedIndex == 0;
   downbtn.disabled = ! listbox.selectedItem || listbox.selectedIndex == listbox.getRowCount() - 1;
-
 }
 
 function copyPrefsToList() {
