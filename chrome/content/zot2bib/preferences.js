d3f9ecedddb2015b5667fe6c343a1dfd7d9b8da3
George MacKerron <george@mackerron.co.uk>
2008-04-03 12:44:52 +0000

    Updates

--- chrome/content/zot2bib/preferences.js
+++ chrome/content/zot2bib/preferences.js
@@ -1,4 +1,7 @@
 
+var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+var browserWindow = wm.getMostRecentWindow("navigator:browser");
+
 function listAction(e) {
 
   const nsIFilePicker = Components.interfaces.nsIFilePicker;
@@ -12,39 +15,39 @@ function listAction(e) {
 
   if (e && e.type == 'command' ) {
 
-  var olditem, newitem;
+    var olditem, newitem;
 
-  if (e.target == addbtn) {
-    var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
-    fp.init(window, "Choose BibTeX file", nsIFilePicker.modeOpen);
-    fp.appendFilter("BibTeX", "*.bib");
-    var rv = fp.show();
-    if (rv == nsIFilePicker.returnOK) {
-      var path = fp.file.path;
-      for (var i = 0; i < listbox.getRowCount(); i ++) {
-        olditem = listbox.getItemAtIndex(i);
-        if (olditem.label == path) {
-          newitem = olditem;
-          break;
+    if (e.target == addbtn) {
+      var fp = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+      fp.init(window, "Choose BibTeX file", nsIFilePicker.modeOpen);
+      fp.appendFilter("BibTeX", "*.bib");
+      var rv = fp.show();
+      if (rv == nsIFilePicker.returnOK) {
+        var path = fp.file.path;
+        for (var i = 0; i < listbox.getRowCount(); i ++) {
+          olditem = listbox.getItemAtIndex(i);
+          if (olditem.label == path) {
+            newitem = olditem;
+            break;
+          }
         }
+        if (! newitem) newitem = listbox.appendItem(path);
       }
-      if (! newitem) newitem = listbox.appendItem(path);
+
+    } else if (e.target == removebtn) {
+      listbox.removeItemAt(listbox.selectedIndex);
+
+    } else if (e.target == upbtn || e.target == downbtn) {
+      olditem = listbox.selectedItem;
+      newitem = listbox.getItemAtIndex(listbox.selectedIndex + ( e.target == upbtn ? -1 : 1 ));
+      [olditem.label, newitem.label] = [newitem.label, olditem.label];
     }
 
-  } else if (e.target == removebtn) {
-    listbox.removeItemAt(listbox.selectedIndex);
-
-  } else if (e.target == upbtn || e.target == downbtn) {
-    olditem = listbox.selectedItem;
-    newitem = listbox.getItemAtIndex(listbox.selectedIndex + ( e.target == upbtn ? -1 : 1 ));
-    [olditem.label, newitem.label] = [newitem.label, olditem.label];
-  }
-
-  if (newitem) {
-    listbox.selectItem(newitem);
-    listbox.ensureElementIsVisible(newitem);
-  }
-  copyListToPrefs();
+    if (newitem) {
+      listbox.selectItem(newitem);
+      listbox.ensureElementIsVisible(newitem);
+    }
+    copyListToPrefs();
   }
 
   removebtn.disabled = ! listbox.selectedItem;
