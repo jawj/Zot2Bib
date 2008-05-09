015a42d85ec3e714fce3be98b60cce1b5349ebd7
George MacKerron <george@mackerron.co.uk>
2008-05-09 14:49:59 +0000

    Removed wrongly tagged 1.0.3 release (actually modified trunk)

--- 1.0.3/chrome/content/zot2bib/include.js
+++
@@ -1,6 +0,0 @@
-// Only create main object once
-if (!Zotero.Zot2Bib) {
-  const loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
-          .getService(Components.interfaces.mozIJSSubScriptLoader);
-  loader.loadSubScript("chrome://zot2bib/content/zot2bib.js");
-}
