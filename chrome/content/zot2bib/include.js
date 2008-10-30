e8fbe0c68ed6a50b6580aacd9fa57254a055c4bc
George MacKerron <george@mackerron.co.uk>
2008-10-30 10:42:09 +0000

    Added modules...

--- chrome/content/zot2bib/include.js
+++ chrome/content/zot2bib/include.js
@@ -1,5 +1,8 @@
-// Only create main object once
-if (!Zotero.Zot2Bib) {
+
+Components.utils.import('resource://zot2bib/zot2bib.js');
+alert(test);
+
+if (!Zotero.Zot2Bib) { // Only create main object once
   const loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
           .getService(Components.interfaces.mozIJSSubScriptLoader);
   loader.loadSubScript("chrome://zot2bib/content/zot2bib.js");
