5666bc7ffa2f5f11ff6c806e556f94537055dda8
George MacKerron <george@mackerron.co.uk>
2011-02-11 11:23:58 +0000

    Typo

--- components/get_ext_dir.js
+++ components/get_ext_dir.js
@@ -4,7 +4,7 @@ GetExtDir.prototype = {
   classDescription: "Extension Directory Component",
   classID:          Components.ID("{723079F5-F880-40BB-8283-8266DEA93960}"),
   contractID:       "@mackerron.com/get_ext_dir;1",
-  QueryInterface:   function { return this; },
+  QueryInterface:   function() { return this; },
   get_ext_dir:      function() {
     var componentFile = __LOCATION__;
     var componentsDir = componentFile.parent;
