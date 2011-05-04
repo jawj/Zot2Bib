90ac833781e124c59e9575f1a894dba0dd8db0a2
George MacKerron <george@mackerron.co.uk>
2011-05-04 17:06:27 +0000

    Oops - untagged non-release 2.0.3

--- mdc-2.0.3/chrome/content/zot2bib/include.js
+++
@@ -1,2 +0,0 @@
-Components.utils.import('resource://zot2bib/zot2bib.jsm');
-Zot2Bib.initOnce(Zotero);
--- mdc-2.0.3/defaults/preferences/defaultprefs.js
+++
@@ -1,8 +0,0 @@
-pref("extensions.z2b.keepinzotero", true);
-pref("extensions.z2b.addtoempty", false);
-pref("extensions.z2b.bibfiles", "");
-pref("extensions.z2b.destfiles", "");
-pref("extensions.z2b.manydests", false);
-pref("extensions.z2b.extrabraces", false);
-pref("extensions.z2b.openpub", true);
-pref("extensions.z2b.bringtofront", true);
